66fe73dbb64e38ba397ff29d1051912e080497d6
Alessioch <alessiochf@gmail.com>
2021-02-14 12:22:27 +0000

    title amend

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,7 @@ export default {
   },
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'artistlab.com',
+    title: '8piddi',
     meta: [
       {
         charset: 'utf-8',
